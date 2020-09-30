54c984ef06d066b472fb89dbc623db5c5888bb5d
Maita Tomoya <58167405+mt114ran@users.noreply.github.com>
2020-10-01 00:07:42 +0900

    add 4file

---
+++ sum_3num.py
@@ -0,0 +1,9 @@
+num1 = input("１つ目の数字を入力してください >>>")
+num2 = input("２つ目の数字を入力してください >>>")
+num3 = input("３つ目の数字を入力してください >>>")
+
+sum1 = num1 + num2 + num3
+print("３つの数字の合計は {} です".format(sum1))
+
+sum2 = int(num1) + int(num2) + int(num3)
+print("３つの数字の合計は {} です".format(sum2))
